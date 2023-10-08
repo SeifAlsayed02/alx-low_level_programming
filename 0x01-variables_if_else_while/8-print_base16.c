65aa2bee3d1c31c5dcf152374cd2fc51de79efdf
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2023-10-08 16:27:07 -0700

    finito

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char c;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
