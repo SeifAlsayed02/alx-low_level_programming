a4078053ed343d7ee36211c75c4e01f7a6bfa63a
root <root@DESKTOP-O1O9OR8>
2023-10-11 02:28:57 +0300

    yala

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int x;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
-		print_alphabet();
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
 	}
-
-
-
-
+	_putchar('\n');
+	}
 }
