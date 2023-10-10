6cb69b33208722ac413d383bd7cf620eedbdc99f
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2023-10-10 15:23:25 -0700

    kolo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char c[] = "_putchar";
+
+
+	int size = sizeof(c);
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+
+}
