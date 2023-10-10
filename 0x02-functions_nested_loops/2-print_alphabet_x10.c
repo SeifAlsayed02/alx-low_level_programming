e3ea9c8aeb225f8fc80f61648fa4c344a05b421a
root <root@DESKTOP-O1O9OR8>
2023-10-11 02:14:36 +0300

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: None.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	void print_alphabet(void);
+	}
+
+
+
+
+}
