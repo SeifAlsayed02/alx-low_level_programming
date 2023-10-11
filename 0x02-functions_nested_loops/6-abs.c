a1d8f8202de181b312796675054f95775dff3f30
root <root@DESKTOP-O1O9OR8>
2023-10-11 03:10:47 +0300

    kk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - get the absolute of the number
+ * @n: The character to get abs
+ *
+ * Return: On success 1.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+
+
+
+}
