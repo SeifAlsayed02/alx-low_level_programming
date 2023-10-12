372c0245e8d4a7035430dd0a52f784bc4e2c4b22
root <root@DESKTOP-O1O9OR8>
2023-10-13 01:19:04 +0300

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: None.
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('c');
+}
