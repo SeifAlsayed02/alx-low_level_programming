3a00c5010f0a8750398e19ef860233f35d26348c
root <root@DESKTOP-O1O9OR8>
2023-10-16 16:01:11 +0300

    test

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d, ", *(a + i));
+		printf("%d", a[i]);
+		printf(", ");
 	}
-	_putchar(a[n - 1]);
-	_putchar('\n');
+	printf(a[n - 1]);
+	printf('\n');
 }
