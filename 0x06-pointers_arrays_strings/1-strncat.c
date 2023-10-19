adf6d12563ff48852706795b20d80d945cde3dbd
root <root@DESKTOP-O1O9OR8>
2023-10-20 02:00:43 +0300

    Tasks

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncat - Entry point
+ * @src: The poniter of type char
+ * @dest: The poniter of type char
+ * @n: Number
+ *
+ * Return: None.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' || j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+
+}
