774a156c2e65153987befc0ab49f84131616d23e
root <root@DESKTOP-O1O9OR8>
2023-10-20 01:47:13 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strcat - Entry point
+ * @src: The poniter of type char
+ * @dest: The poniter of type char
+ *
+ * Return: None.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	i--;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	src[j + 1] = '\0';
+	return (dest);
+
+}
