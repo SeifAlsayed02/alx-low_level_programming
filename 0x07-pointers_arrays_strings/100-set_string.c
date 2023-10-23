154538a756ce4f2426672d5abeb5152306f0d71c
root <root@DESKTOP-O1O9OR8>
2023-10-24 02:45:04 +0300

    Tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ * @to: The poniter of type char
+ * @s: The poniter of type char
+ *
+ * Return: None.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+/**
+ * print_diagsums - Entry point
+ * @a: The poniter of type int
+ * @size: Number
+ *
+ * Return: None.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int main_diagonal = 0;
+	int right_diagonal = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		main_diagonal += a[i + (size * i)];
+		right_diagonal += a[(size - 1) + (size - 1) * i];
+	}
+
+	printf("%d, %d\n", main_diagonal, right_diagonal);
+
+}
