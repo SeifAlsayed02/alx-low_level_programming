2d50712b0a356395243eba57c2a3b9661e155b7a
root <root@DESKTOP-O1O9OR8>
2023-10-24 00:13:58 +0300

    Tasks

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - Entry point
+ * @s: The poniter of type char
+ * @b: The poniter of type char
+ * @n: Number
+ *
+ * Return: None.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	retutn(s);
+}
