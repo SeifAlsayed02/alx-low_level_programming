1cd925247495564d31d9e67af136ed18dc2baf72
root <root@DESKTOP-O1O9OR8>
2023-10-25 23:58:06 +0300

    Tasks

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_rev_recursion - Entry point
+ * @s: The poniter of type char
+ *
+ * Return: None.
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+		return;
+
+	_puts_recursion(s + 1);
+	_putchar(*s);
+
+}
