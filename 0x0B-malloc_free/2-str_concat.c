9510a5076821b55e3aabec8fcb25e03dec00232f
root <root@DESKTOP-O1O9OR8>
2023-10-30 21:28:50 +0200

    Tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,12 @@ char *str_concat(char *s1, char *s2)
 	int size;
 	char *c;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	else if (s1 == NULL && s2 != NULL)
+		return (s2);
+	else if (s2 == NULL && s1 != NULL)
+		return (s1);
 
 	while (s1[size1] != '\0')
 		size1++;
