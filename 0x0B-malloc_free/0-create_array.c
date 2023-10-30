85aa601aba10db2f089842e9d053c8eb786f4282
root <root@DESKTOP-O1O9OR8>
2023-10-30 17:50:14 +0200

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *create_array - Entry point
+ * @size: int
+ * @c: variable Char
+ *
+ * Return: None.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr = malloc(size);
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
