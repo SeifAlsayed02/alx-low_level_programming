e2f54f15b045fb6a6abf2edd7f32363b0dd1f1b9
root <root@DESKTOP-O1O9OR8>
2023-10-30 21:24:31 +0200

    Tasks

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * *str_concat - Entry point
+ * @s1: The poniter of type char
+ * @s2: The poniter of type char
+ *
+ * Return: None.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j;
+	int size1 = 0;
+	int size2 = 0;
+	int size;
+	char *c;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+	size = size1 + size2;
+
+	c = malloc(size * sizeof(*str) + 1);
+
+	if (c == 0)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		c[i] = s1[i];
+	for (j = 0; j < size2; j++)
+	{
+		c[i] = s2[j];
+		i++;
+	}
+
+	return (c);
+}
