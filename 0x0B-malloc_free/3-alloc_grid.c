0bf16b0285243a4f4a2e37081cb586e74d66853d
root <root@DESKTOP-O1O9OR8>
2023-11-01 00:35:23 +0200

    Tasks

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * **alloc_grid - Entry point
+ * @width: The width
+ * @height: The height
+ *
+ * Return: None.
+ */
+int **alloc_grid(int width, int height)
+{
+
+	int i;
+	int **ptr = NULL;
+
+	if (width <= 0 || heigth <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) height);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(4 * width);
+
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+
+	return (ptr);
+}
