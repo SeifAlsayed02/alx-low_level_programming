4da3305981f15bf29129b959584646d1bc29f4e2
root <root@DESKTOP-O1O9OR8>
2023-11-02 13:48:33 +0200

    Tasks

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * *string_nconcat - Entry point
+ * @s1: pointer of char
+ * @s2:pointer of char
+ * @n: size of s2
+ *
+ * Return: None.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i;
+	unsigned int j = 0;
+	unsigned int size1 = 0;
+	unsigned int size2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1 != '\0')
+		size1++;
+
+	while (s2 != '\0')
+		size2++;
+
+
+	if (n >= size2)
+		n = size2;
+	ptr = malloc(size1 + n + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+
+	for (; i < (size1 + n); i++)
+	{
+		ptr[i] = s2[j];
+		j++
+	}
+	return (ptr);
+
+
+}
