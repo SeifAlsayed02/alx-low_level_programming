bc2426b709e28f30cedfb134658f84281068d0c8
root <root@DESKTOP-O1O9OR8>
2023-11-07 00:36:18 +0200

    Tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - Entry point
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
