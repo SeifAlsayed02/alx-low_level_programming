d673bba6d1221aae77384f003829a94f34d7ba0e
root <root@DESKTOP-O1O9OR8>
2023-11-17 00:54:29 +0200

    Tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: List
+ *
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	char *tmp;
+
+	while (h != NULL)
+	{
+		printf("[%d]", h->len);
+		tmp = h->str;
+		if (tmp == NULL)
+			printf("%s\n", "(nil)");
+		printf("%s\n", tmp);
+		h = h->next;
+		count++;
+	}
+	return (count);
+
+
+}
