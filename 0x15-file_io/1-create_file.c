59099368558004bfd6d77bff9777b8183142e6ba
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2023-11-27 23:25:32 +0200

    Tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file = -1;
-	int length;
 
 	if (filename == NULL)
 		return (file);
-
-	if (text_content == NULL)
-		return (file);
-	length = strlen(text_content);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
 
-	write(file, text_content, length);
+	if (text_content == NULL)
+		return (-1);
+
+	write(file, text_content, strlen(text_content));
 
 	return (1);
 
