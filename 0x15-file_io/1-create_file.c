89340079947e0d4d0daae41721daedee354f8324
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2023-11-27 23:21:05 +0200

    Tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: file name
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file = -1;
+
+	if (filename == NULL)
+		return (file);
+
+	if (text_content == NULL)
+		return (file);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file == NULL)
+		return (-1);
+
+	write(file, text_content, strlen(text_content));
+
+	return (1);
+
+}
