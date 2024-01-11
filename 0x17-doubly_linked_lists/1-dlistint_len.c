cd653b9e2b4c52f388650611f87c0acbe4a5f572
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-01-11 21:21:20 +0200

    Tasks

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list.
+ * @h: The head
+ *
+ * Return: The number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	dlistint_t *node;
+
+	if (h == NULL)
+		return (0);
+
+	node = h->next;
+	while (node != NULL)
+	{
+		node = node->next;
+		size++;
+	}
+	return (size);
+}
