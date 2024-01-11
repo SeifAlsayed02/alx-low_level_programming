6eff88a17c23e6b2f171c06a1c8eb08d8e1ea659
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-01-11 22:38:57 +0200

    Updating

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -36,7 +36,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (current->next != NULL)
 		current->next->prev = temp;
-	ree(current);
+	free(current);
 	return (1);
 }
 
