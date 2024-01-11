c8f9ec23bfd3cbef7162aa223e30403a98377f44
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-01-11 22:20:50 +0200

    Tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -12,7 +12,7 @@ void free_dlistint(dlistint_t *head)
 
 	if (head == NULL)
 		return;
-	
+
 	node = head;
 	while (node != NULL)
 	{
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,20 +1,25 @@
 #include "lists.h"
 /**
  * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
- * @h: The head
+ * @head: The head
  * @index: index of the node to retrieve (starting from 0)
  *
  * Return: None.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 {
-	unsigned int i;
-	dlistint_t *current = head;
-	
-	for (i = 0; current != NULL && i < index; i++)
+	unsigned int i = 0;
+	dlistint_t *current = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	current = head;
+	while (current != NULL && i != index)
 	{
+		i++;
 		current = current->next;
 	}
-	
+
 	return (current);
 }
