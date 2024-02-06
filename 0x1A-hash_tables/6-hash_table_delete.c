477c5b7ff50091c50c500192513f38601ebaf3e2
SeifAlsayed02 <144794216+SeifAlsayed02@users.noreply.github.com>
2024-02-06 20:53:56 +0200

    Tasks

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - delete hash table
+* @ht: hash table
+*
+* Return: None
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
